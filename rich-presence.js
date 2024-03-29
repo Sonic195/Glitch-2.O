690af119253da2bb66cad85034e7b05f597a526f
Glitch (the-bot-glitch-2-o) <none>
2024-03-29 13:29:37 +0000

    Rewound to commit 846bb3c61ab01523f537a22fb5efdbd7db9da7b7 + 0 edits

--- rich-presence.js
+++ rich-presence.js
@@ -1,6 +1,5 @@
-import "dotenv";
-
-const { Client, IntentsBitField } = require("discord.js");
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
 
 const client = Client({
   intents: [
@@ -11,12 +10,12 @@ const client = Client({
   ],
 });
 
-client.on("ready", (c) => {
+client.on('ready', (c) => {
   console.log(`✅ ${c.user.tag} is online.`);
-
+  
   client.user.setActivity({
-    name: "On Fire",
-  });
+    name: 'On Fire'
+  })
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN)
