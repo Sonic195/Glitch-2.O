53f1dca89af7ec5840b92e76f3f712f39e8fb248
Glitch (the-bot-glitch-2-o) <none>
2024-03-29 10:05:45 +0000

    💽🏡 Checkpoint ./rich-presence.js:51646777/321

--- rich-presence.js
+++ rich-presence.js
@@ -1,1 +1,15 @@
-require('dotenv').config()
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', (c) => {
+  console.log(' ')
+})
