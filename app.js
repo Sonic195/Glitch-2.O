8c36f3f315432a4fa855e0ed62d5129911594bbb
Glitch (the-bot-glitch-2-o) <none>
2024-03-31 08:55:42 +0000

    Rewound to commit cf0a5b5b85b5fdd38ce46c4a12dbb4ae448c666d + 0 edits

--- app.js
+++ app.js
@@ -63,21 +63,14 @@ app.post('/interactions', async function (req, res) {
       });
     }
     if (name === 'flip a coin') {
-      const selectedOption = req.data.options?.find(option => option.name === 'biasness');
-      let responseContent;
-      if (selectedOption) {
-        const selectedValue = selectedOption.value;
-        return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `${selectedValue} wins`,
-      } 
-      });
-    } else {
+      // Send a message into the channel where co
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-          content: `${selectedValue} wins`,
+          // Fetches a random emoji to send from a helper function
+          content: "",
+        },
+      });
     }
     // "challenge" command
     if (name === 'challenge' && id) {
