e3d1015378a2b9ca65bc7a06850a9a1c5dcb38b9
Glitch (the-bot-glitch-2-o) <none>
2024-04-01 09:18:13 +0000

    ⛴💾 Checkpoint ./index.js:51646777/372

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const { Client, IntentsBitField }
+
+const client = new Client({
+  intents:
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', (c) => {
+  console.log(`${c.user.tag} is online`);
+  
+})
+
+client.login(process.env.DISCORD_TOKEN)
